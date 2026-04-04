96ebc64decb234c630c6cb136a4148c1f112e155
omkar <omkarwagh272@gmail.com>
2026-04-04 14:52:06 +0530

    updated Notification repository

--- src/main/java/com/freelancego/repo/NotificationRepository.java
+++ src/main/java/com/freelancego/repo/NotificationRepository.java
@@ -8,7 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
     @Query("""
         SELECT n FROM Notification n
