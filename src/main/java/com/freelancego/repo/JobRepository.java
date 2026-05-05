5e7982762aa1c84359d192be0ecea3c3af8e4d03
omkar <omkarwagh272@gmail.com>
2026-05-05 13:10:53 +0530

    added method to get freelancer with pagination in jobRepo

--- src/main/java/com/freelancego/repo/JobRepository.java
+++ src/main/java/com/freelancego/repo/JobRepository.java
@@ -2,6 +2,7 @@ package com.freelancego.repo;
 
 import com.freelancego.enums.JobPhase;
 import com.freelancego.enums.JobStatus;
+import com.freelancego.model.ChatMessage;
 import com.freelancego.model.Client;
 import com.freelancego.model.Freelancer;
 import com.freelancego.model.Job;
@@ -18,23 +19,34 @@ import java.util.List;
 
 public interface JobRepository extends JpaRepository<Job, Integer> {
     Page<Job> findJobByClient(Client client, Pageable pageable);
+
     List<Job> findByClientIdAndStatusAndPhaseIn(int id, JobStatus status, List<JobPhase> phases);
+
     List<Job> findByClientIdAndStatusAndPhase(int id, JobStatus status, JobPhase phases);
+
     List<Job> findByClientIdAndStatus(int clientId, JobStatus status);
+
     List<Job> findByClientIdAndPhase(int id, JobPhase jobPhase);
+
     Page<Job> findByStatus(Pageable pageable, JobStatus jobStatus);
+
     Page<Job> findJobByClientAndStatus(Client client, JobStatus jobStatus, Pageable pageable);
+
     Page<Job> findByClient(Client client, Pageable pageable);
+
+    Page<Job> findByFreelancer(Freelancer freelancer, Pageable pageable);
+
     List<Job> findByClient(Client client);
+
     Page<Job> findJobByStatus(JobStatus jobStatus, Pageable pageable);
 
     @Query("""
-       SELECT DISTINCT j 
-       FROM Job j 
-       JOIN j.bids b 
-       WHERE b.freelancer.id = :freelancerId
-       ORDER BY j.createdAt DESC
-       """)
+            SELECT DISTINCT j 
+            FROM Job j 
+            JOIN j.bids b 
+            WHERE b.freelancer.id = :freelancerId
+            ORDER BY j.createdAt DESC
+            """)
     Page<Job> findJobsBidByFreelancer(@Param("freelancerId") int freelancerId, Pageable pageable);
 
     @Query("SELECT COUNT(j) FROM Job j JOIN j.bids b WHERE j.status = :status AND b.freelancer.id = :freelancerId")
@@ -45,5 +57,4 @@ public interface JobRepository extends JpaRepository<Job, Integer> {
     @Query("UPDATE Job j SET j.status = :inactiveStatus WHERE j.createdAt <= :thresholdDate")
     void setJobStatusToInactive(@Param("thresholdDate") OffsetDateTime thresholdDate,
                                 @Param("inactiveStatus") JobStatus inactiveStatus);
-
 }
